95217e0cee12fd9457a789c396970cd45153e3d7
Nicholas Lum <nicholaslum444@hotmail.com>
2017-04-19 05:09:39 -0400

    skeleton code done
    server responds to all endpoints except login and logout

--- server.js
+++ server.js
@@ -18,40 +18,104 @@ var multer  = require('multer')
 
 var app = express();
 var upload = multer({ 'dest': __dirname + '/uploads/tmp' });
-app.use(bodyParser.urlencoded({limit:IMAGE_SIZE_LIMIT, extended:false}));
+app.use(bodyParser.urlencoded({limit:IMAGE_SIZE_LIMIT, extended:true}));
 app.use(bodyParser.json({limit:IMAGE_SIZE_LIMIT}));
 
 // --- ROUTES ---
 
 // root
-app.get('/', root);
+app.get('/', rootHandler);
 
 // blacklist
-app.get('/blacklist/keys', getAllBlacklistKeysHandler);
-app.get('/blacklist/img', getBlacklistImageFileHandler);
 app.post('/blacklist/add/url', addUrlToBlacklistHandler);
 app.post('/blacklist/add/img', upload.single('image_file'), addImageFileToBlacklistHandler);
+app.get('/blacklist/keys', getAllBlacklistKeysHandler);
+app.get('/blacklist/img/info', getBlacklistImageInfoHandler);
+app.post('/blacklist/img/edit', editBlacklistImageHandler);
+app.post('/blacklist/img/delete', deleteBlacklistImageHandler);
+app.get('/blacklist/img', getBlacklistImageFileHandler);
+
+// compare
+app.post('/compare', compareHandler);
+
+// settings
+app.get('/settings', getSettingsHandler);
+app.post('/settings/update', updateSettingsHandler);
 
 // --- HANDLERS ---
 
 // root
-function root(request, response) {
+function rootHandler(request, response) {
     response.json([0,1,2,3,4]);
+    return;
+}
+
+// for POST 'blacklist/add/url?auth_token=asdasd'
+// blacklists the image file at the given url
+function addUrlToBlacklistHandler(request, response) {
+    var authToken = request.query.auth_token;
+    var imageUrl = request.body.image_url;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    if (!isValidUrl(imageUrl)) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'not a valid url'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var imageKey = addUrlToBlacklist(imageUrl);
+    var data = {image_key:imageKey};
+    var responseObj = createResponseObj('success', data);
+    response.json(responseObj);
+    return;
+}
+
+// for POST 'blacklist/add/img?auth_token=asdasd'
+// blacklists the image file as uploaded
+function addImageFileToBlacklistHandler(request, response) {
+    var authToken = request.query.auth_token;
+    var file = request.file;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    if (!isValidImageFile(file)) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'file not a valid image'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var imageKey = addImageFileToBlacklist(file);
+    var data = {image_key:imageKey};
+    var responseObj = createResponseObj('success', data);
+    response.json(responseObj);
+    return;
 }
 
 // for GET '/blacklist/keys?auth_token=asdad'
 // gets all the keys in the user's blacklist
 function getAllBlacklistKeysHandler(request, response) {
     var authToken = request.query.auth_token;
-    if (isValidAuthToken(authToken)) {
-        var username = getUsernameFromAuthToken(authToken);
-        var userBlacklistKeys = getUserBlacklistKeys(username);
-        var responseObj = createResponseObj('success', userBlacklistKeys);
-        response.json(responseObj);
-    } else {
+    
+    if (!isValidAuthToken(authToken)) {
         var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
         response.json(responseObj);
+        return;
     }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    var userBlacklistKeys = getUserBlacklistKeys(username);
+    var responseObj = createResponseObj('success', userBlacklistKeys);
+    response.json(responseObj);
+    return;
 }
 
 // for GET 'blacklist/img?auth_token=asdasd&image_key=qweqwe'
@@ -59,61 +123,239 @@ function getAllBlacklistKeysHandler(request, response) {
 function getBlacklistImageFileHandler(request, response) {
     var authToken = request.query.auth_token;
     var imageKey = request.query.image_key;
-    if (isValidAuthToken(authToken)) {
-        if (isValidImageKey(imageKey)) {
-            var username = getUsernameFromAuthToken(authToken);
-            var filepath = getFilepathFromImageKey(imageKey);
-            response.sendFile(filepath);
-        } else {
-            var responseObj = createResponseObj('fail', null, {code:404, message:'image key not valid'})
-            response.json(responseObj);
-        }
-    } else {
+    
+    if (!isValidAuthToken(authToken)) {
         var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
         response.json(responseObj);
+        return;
     }
+    
+    if (!isValidImageKey(imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:404, message:'image key not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    if (!hasPermission(username, imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:403, message:'no permission to access image'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var filepath = getFilepathFromImageKey(imageKey);
+    response.sendFile(filepath);
+    return;
 }
 
-// for POST 'blacklist/add/url?auth_token=asdasd'
-// blacklists the image file at the given url
-function addUrlToBlacklistHandler(request, response) {
+function editBlacklistImageHandler(request, response) {
     var authToken = request.query.auth_token;
-    if (isValidAuthToken(authToken)) {
-        var username = getUsernameFromAuthToken(authToken);
-        var imageUrl = request.body.image_url;
-        var imageKey = addUrlToBlacklist(imageUrl);
-        var data = {image_key:imageKey};
-        var responseObj = createResponseObj('success', data);
-        response.json(responseObj);
-    } else {
+    var imageKey = request.query.image_key;
+    var imageInfo = request.body.image_info;
+    
+    if (!isValidAuthToken(authToken)) {
         var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
         response.json(responseObj);
+        return;
     }
+    
+    if (!isValidImageKey(imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:404, message:'image key not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    if (!hasPermission(username, imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:403, message:'no permission to access image'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var editSuccessful = editImage(imageKey, imageInfo);
+    if (!editSuccessful) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'edit failed'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var responseObj = createResponseObj('success', editSuccessful);
+    response.json(responseObj);
+    return;
 }
 
-// for POST 'blacklist/add/img?auth_token=asdasd'
-// blacklists the image file as uploaded
-function addImageFileToBlacklistHandler(request, response) {
+function deleteBlacklistImageHandler(request, response) {
     var authToken = request.query.auth_token;
-    if (isValidAuthToken(authToken)) {
-        var username = getUsernameFromAuthToken(authToken);
-        var file = request.file;
-        if (isValidImageFile(file)) {
-            var imageKey = addImageFileToBlacklist(file);
-            var data = {image_key:imageKey};
-            var responseObj = createResponseObj('success', data);
-            response.json(responseObj);
-        } else {
-            var responseObj = createResponseObj('fail', null, {code:500, message:'file not valid'})
-            response.json(responseObj);
-        }
-    } else {
+    var imageKey = request.query.image_key;
+    
+    if (!isValidAuthToken(authToken)) {
         var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
         response.json(responseObj);
+        return;
     }
+    
+    if (!isValidImageKey(imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:404, message:'image key not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    if (!hasPermission(username, imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:403, message:'no permission to access image'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var deleteSuccessful = deleteImage(imageKey);
+    if (!deleteSuccessful) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'delete failed'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var responseObj = createResponseObj('success', deleteSuccessful);
+    response.json(responseObj);
+    return;
+}
+
+function getBlacklistImageInfoHandler(request, response) {
+    var authToken = request.query.auth_token;
+    var imageKey = request.query.image_key;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    if (!isValidImageKey(imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:404, message:'image key not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    if (!hasPermission(username, imageKey)) {
+        var responseObj = createResponseObj('fail', null, {code:403, message:'no permission to access image'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var imageInfo = getImageInfo(imageKey);
+    var responseObj = createResponseObj('success', imageInfo);
+    response.json(responseObj);
+    return;
+}
+
+function compareHandler(request, response) {
+    var authToken = request.query.auth_token;
+    var imageUrl = request.body.image_url;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    if (!isValidUrl(imageUrl)) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'not a valid url'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var similarityInfo = compare(imageUrl);
+    var responseObj = createResponseObj('success', similarityInfo);
+    response.json(responseObj);
+    return;
+}
+
+function getSettingsHandler(request, response) {
+    var authToken = request.query.auth_token;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    var userSettings = getUserSettings(username);
+    var responseObj = createResponseObj('success', userSettings);
+    response.json(responseObj);
+    return;
+}
+
+function updateSettingsHandler(request, response) {
+    var authToken = request.query.auth_token;
+    var settings = request.body.settings;
+    
+    if (!isValidAuthToken(authToken)) {
+        var responseObj = createResponseObj('fail', null, {code:401, message:'auth token not valid'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var username = getUsernameFromAuthToken(authToken);
+    var updateSuccessful = updateSettings(username, settings);
+    if (!updateSuccessful) {
+        var responseObj = createResponseObj('fail', null, {code:500, message:'settings update failed'})
+        response.json(responseObj);
+        return;
+    }
+    
+    var responseObj = createResponseObj('success', updateSuccessful);
+    response.json(responseObj);
+    return;
 }
 
 // --- HELPER FUNCTIONS ---
+
+// TODO implement permission check for images
+function hasPermission(username, imageKey) {
+    return true;
+}
+
+// TODO implement comparision for image
+function compare(imageKey) {
+    return [{
+        image_key: '78e67d8f9a569db686c54c8e67.jpg',
+        similarity: 0.9
+    },{
+        image_key: '88e6769db686d8f9a5c54c8e67.jpg',
+        similarity: 0.2
+    }];
+}
+
+// TODO implement get user's settings
+function getUserSettings(username) {
+    return {
+        sites_ignored: [
+            'url_of_site_1', 
+            'url_of_site_2'
+        ]
+    }
+}
+
+// TODO implement update settings, and return success true
+function updateSettings(username, settings) {
+    return true;
+}
+
+// TODO implement get image info
+function getImageInfo(imageKey) {
+    return {image_keywords: ['keywords', 'of', 'image', imageKey]}
+}
+
+// TODO implement edit image, and return success true
+function editImage(imageKey, imageInfo) {
+    return true;
+}
+
+// TODO implement delete image, and return success true
+function deleteImage(imageKey) {
+    return true;
+}
 
 // TODO implement add url to blacklist
 function addUrlToBlacklist(imageUrl) {
@@ -125,7 +367,7 @@ function addImageFileToBlacklist(file) {
     return 'image-key-af8163d7e9a999';
 }
 
-// TODO replace with actual functionality
+// TODO implement get blacklist of user
 function getUserBlacklistKeys(username) {
     return ['blacklist', 'of', 'images', username]
 }
@@ -148,21 +390,18 @@ function isValidImageFile(file) {
     }
 }
 
+// TODO check if url is valid
+function isValidUrl(url) {
+    if (url) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // TODO implement get by image key
 function getFilepathFromImageKey(imageKey) {
     return __dirname + '/uploads/test.jpg';
-}
-
-// creates the response object to be returned in api
-function createResponseObj(status, data, fail) {
-    var responseObj = {};
-    responseObj.status = status;
-    if (status === 'success') {
-        responseObj.data = data;
-    } else {
-        responseObj.fail = fail
-    }
-    return responseObj;
 }
 
 // TODO replace with actual validity check
@@ -177,6 +416,18 @@ function isValidAuthToken(authToken) {
 // TODO replace with actual get
 function getUsernameFromAuthToken(authToken) {
     return 'nick';
+}
+
+// creates the response object to be returned in api
+function createResponseObj(status, data, fail) {
+    var responseObj = {};
+    responseObj.status = status;
+    if (status === 'success') {
+        responseObj.data = data;
+    } else {
+        responseObj.fail = fail
+    }
+    return responseObj;
 }
 
 // --- LISTEN ---
